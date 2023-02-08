b9ab805c613cef518cc8b284fd21eee27711ec27
rodrigoaster <rodrigowebx@gmail.com>
2023-02-08 20:49:46 -0300

    home: create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,43 @@
-import { Heading } from "../components/Heading";
+import { Body, Img, TextContainer } from "./styles";
+
+import ProfileImg from '../../public/profile.png'
 
 export function Home() {
-    return (
-        <Heading />
+  const variantsImg = {
+    first: { opacity: 0 },
+    last: { opacity: 1 }
+  }
+  
+  const variantsTextBody = {
+    first: { opacity: 0, y: 50 },
+    last: { opacity: 1, y: 0 }
+  }
+
+  return (
+    <Body>
+      <Img 
+        src={ProfileImg} 
+        alt="profile photo of developer"
+        initial="first"
+        whileInView="last"
+        viewport={{ once: true, amount: 0.8 }}
+        variants={variantsImg}
+        transition={{ type: "spring", stiffness: 20, delay: 2 }}
+      />
+      <TextContainer
+        initial="first"
+        whileInView="last"
+        viewport={{ once: true, amount: 0.8 }}
+        variants={variantsTextBody}
+        transition={{ type: "spring", stiffness: 20, delay: 3 }}
+      >
+        <h1>Rodrigo Aster</h1><br />
+        <h2>I am Software Developer at <a href="https://www.accenture.com/br-pt" target="_blank">@Accenture Brasil</a></h2><br />
+        <p>Hi, welcome to my little world.</p><br />
+        <p>I'm started my studies in Web Development a one year ago.</p><br />
+        <p>Today, I'm working at Accenture Brasil as a Software Developer.</p><br />
+        <p>Let's code! 😆</p>
+      </TextContainer>
+    </Body>
     );
-}
+  }
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,5 +1,52 @@
 import styled from "styled-components";
+import { motion } from 'framer-motion'
 
-export const HomeStyle = styled.div`
-    
+export const Body = styled(motion.div)`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  color: #fff;
+
+  height: 100vh;
+
+  padding-top: 50px;
+
+  h1 {
+    padding-top: 72px;
+  }
+
+`
+
+export const Img = styled(motion.img)`
+  width: 25rem;
+  height: 31.25rem;
+
+  margin-top: 20px;
+
+  border-radius: 100px;
+`
+
+export const TextContainer = styled(motion.p)`
+  width: 38%;
+  
+  p {
+    width: 80%;
+
+    color: #808080;
+
+    font-size: 24px;
+    font-weight: 300;
+  }
+
+  a {
+    text-decoration: none;
+
+    color: #fff;
+
+    :hover {
+      color: #808080;
+      transition: .5s ease-in-out;
+    }
+  }
 `
