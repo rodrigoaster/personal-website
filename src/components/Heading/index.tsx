16679e7f53ebcc5375789db0abfec1f09e030286
Rodrigo Aster <rodrigowebx@gmail.com>
2023-03-03 11:23:49 -0300

    contextTheme: use Context and global variables os themes

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,8 +1,19 @@
+import { useContext } from 'react';
+
 import { Container, Logo, Navbar, Navlink, Link } from './styles';
 
-import imgLogo from '../../../public/logo.png'
+import Switch from 'react-switch'
 
-export function Heading() {
+import img_logo_dark from '../../../public/logo.png'
+import img_logo_light from '../../../public/logo-light.png'
+
+import { ThemeContext } from 'styled-components';
+
+interface HeadingProps {
+  toggleTheme(): void;
+}
+
+export function Heading(props: HeadingProps) {
   const variantsLogo = {
     first: { opacity: 0, y: 70 },
     last: { opacity: 1, y: 0 }
@@ -12,6 +23,8 @@ export function Heading() {
     first: { opacity: 0 },
     last: { opacity: 1 }
   }
+
+  const { colores, title } = useContext(ThemeContext)
 
   return (
     <Container
@@ -28,7 +41,12 @@ export function Heading() {
         variants={variantsLogo}
         transition={{ type: "spring", stiffness: 20, delay: 1.2 }}
       >
-        <img src={imgLogo} alt="logo of header" />
+        {
+          title === 'light' ? 
+          <img src={img_logo_light} alt="logo of header" />
+          :
+          <img src={img_logo_dark} alt="logo of header" />
+        }
       </Logo>
       <nav>
         <Navbar>
@@ -43,6 +61,15 @@ export function Heading() {
           </Navlink>
         </Navbar>
       </nav>
+      <Switch 
+        onChange={props.toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        handleDiameter={20}
+        offColor="#ccc"
+        onColor='#001aff'
+      />
     </Container>
   );
 }
